dadd070a2802e7380cc2884bbd5ee77c182c246f
kzoeps <kzoepa@gmail.com>
2021-11-14 21:21:58 +0600

    feat(dashboard): created folder routes and dummy screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import CheckInNOut from './screens/check-in/check-in-n-out';
 import app from './utils/constants/firebase';
 import LoginContainer from './screens/login/login';
+import { Dashboard } from './screens/dashbaord/dashboard-container';
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<CheckInNOut/>}/>
 				<Route path="/login" element={<LoginContainer/>}/>
+				<Route path="/dashboard" element={<Dashboard/>}/>
 			</Routes>
 		</Router>
 	);
---
+++ src/screens/dashbaord/dashboard-container.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {Typography} from '@mui/material';
+
+export const Dashboard = () => {
+  return (
+    <div>
+      <Typography variant="h3">Dashboard</Typographyv>
+    </div>
+  );
+};
+
+export default Dashboard;
